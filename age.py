5a6bbe510411735c976b2109765a092b5e1132ec
student <aleksandravojcenko@gmail.com>
2023-02-16 18:18:55 +0300

    задача 3

--- age.py
+++ age.py
@@ -1,7 +1,6 @@
 age = int(input('Введите год'))
 print()
-if age % 4 :
+if age % 4 == 0 and age % 100 != 0 and age % 400:
     print(age,'год високосный')
 else:
     print('год не високосный')
-    
